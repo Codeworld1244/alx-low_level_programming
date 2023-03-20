57ebafd5622f367dd2e65825118db2b25f5d798a
codeworld1244 <harerimanainnocent9@gmail.com>
2023-03-20 16:24:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this program prints a line with functions
+ * return:0 (success)
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
