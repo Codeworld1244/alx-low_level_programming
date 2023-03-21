e99aa070455c3d55be4011e20bafbf67e5718db9
codeworld1244 <harerimanainnocent9@gmail.com>
2023-03-20 17:55:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that print a line to a standard error
+ * main - A c program that print a line to a standard e
+ * rror
  * return 1 (success)
  */
 int main(void)
 {
-        printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",stdout);
+        printf("and that piece of art is useful" - Dora Kor
+par, 2015-10-19\n",stdout);
 return (1);
 }
